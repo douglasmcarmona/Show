fe66be80425032fcca1ca24cfe71f390ce9ca5a9
Pertence <pertencem@gmail.com>
2015-03-30 17:53:23 -0300

    1

--- src/Show.java
+++ src/Show.java
@@ -1,5 +1,16 @@
 import java.*;
 
+class Jogador{
+	int id;
+	String Nome;
+	int[] pergunta, resposta;
+}
+
+class Pergunta{
+	int id, resposta;
+	String Pergunta, Resposta1, Resposta2, Resposta3, Resposta4;
+}
+
 public class Show {
 	public static void main (String args[]){
 		
