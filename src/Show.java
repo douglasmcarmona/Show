a3dbea4ad38b965e2d8f11b14b969fc13cbf72f7
Pertence <pertencem@gmail.com>
2015-03-30 18:29:14 -0300

    2

--- src/Show.java
+++ src/Show.java
@@ -1,18 +1,58 @@
-import java.*;
+import java.io.*;
 
-class Jogador{
+class Players{
 	int id;
-	String Nome;
-	int[] pergunta, resposta;
+	String Name;
+	int[] questions, answers;
+	void read() throws IOException {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream("players.txt");
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+	}
+	void write() throws IOException {
+        FileInputStream in = null;
+        try {
+        	in = new FileInputStream("players.txt");
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+	}
 }
 
-class Pergunta{
-	int id, resposta;
-	String Pergunta, Resposta1, Resposta2, Resposta3, Resposta4;
+class Questions{
+	int id, answers;
+	String Questions, Answer1, Answer2, Answer3, Answer4;
+	void read() throws IOException {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream("questions.txt");
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+	}
+	void write() throws IOException {
+        FileInputStream in = null;
+        try {
+        	in = new FileInputStream("questions.txt");
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+	}
 }
 
 public class Show {
-	public static void main (String args[]){
-		
-	}
+    public static void main(String[] args) throws IOException {
+    	
+    }
 }
